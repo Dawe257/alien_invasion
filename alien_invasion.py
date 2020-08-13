6b29b4bf215d0e93684b82d7af6c2d3006586620
David Zhenetl <zhenetl-dm@ao-rr.ru>
2020-08-13 16:41:27 +0300

    update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,50 @@
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         self.bg_color = (230, 230, 230)
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+            
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _update_screen(self):
+        self.screen.fill((0, 0, 0))
+        self.ship.blitme()
+        pygame.display.flip()
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    exit()
-            self.screen.fill(self.settings.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+            self.clock.tick(self.settings.framerate)
 
 
 if __name__ == '__main__':
